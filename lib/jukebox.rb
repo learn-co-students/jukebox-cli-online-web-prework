761f86eaebd798512c2f8531f3b16764f6df2628
Brandon Teich <brandonteich123@gmail.com>
2018-10-15 00:29:54 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,4 +9,39 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help()
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
 
+
+
+
+def play(songs) 
+  input = gets.chomp
+  valid = false
+  puts "Please enter a song name or number:"
+  songs.each_with_index do |key, value|
+    if (input.to_i - 1 == value)
+      puts "Playing #{songs[value]}"
+      valid = true
+    elsif (choice == song)
+      puts "Playing #{input}"
+      valid = true
+    end
+  end
+  if valid == false 
+  puts "Invalid input, please try again" 
+end
+end
+def list (songs) 
+  songs.each.with_index do |key, value| 
+  puts "#{value + 1}. #{key}"
+end
+end
+
+  
+  
