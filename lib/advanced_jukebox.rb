466cb3cdfb23e79e28d395141dc3742d45ac34f0
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-21 18:00:12 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -28,23 +28,48 @@ def list(my_songs)
   end
 end
 
-binding.pry
+#binding.pry
 
 def play(my_songs)
   #this method is slightly different!
   #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
+  #this time, only allow users to input a song name
   #check to see if the name they give is in fact a key of the my_songs hash
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-
+  puts "Please enter a song name:"
+  #binding.pry
+  response = gets.chomp
+  if my_songs.keys.include?(response)
+    puts "Playing #{response}"
+    #binding.pry
+    system "start spotify #{my_songs[response]}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  help
+  loop do
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "help"
+      help
+    when "play"
+      list(my_songs)
+      play(my_songs)
+    when "list"
+      list(my_songs)
+    when "exit"
+      exit_jukebox
+      break
+    end
+  end
 end
