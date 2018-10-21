4928a3a30788dc8cd095068b9e5ed16e9aba87c3
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-21 16:55:37 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,29 +1,34 @@
+require 'pry'
+
 #Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+"Go Go GO" => 'C:/Users/Andrew/.atom/.learn-ide/home/aaj3f/jukebox-cli-online-web-prework/audio/Emerald-Park/01.mp3',
+"LiberTeens" => 'C:/Users/Andrew/.atom/.learn-ide/home/aaj3f/jukebox-cli-online-web-prework/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  'C:/Users/Andrew/.atom/.learn-ide/home/aaj3f/jukebox-cli-online-web-prework/audio/Emerald-Park/03.mp3',
+"Guiding Light" => 'C:/Users/Andrew/.atom/.learn-ide/home/aaj3f/jukebox-cli-online-web-prework/audio/Emerald-Park/04.mp3',
+"Wolf" => 'C:/Users/Andrew/.atom/.learn-ide/home/aaj3f/jukebox-cli-online-web-prework/audio/Emerald-Park/05.mp3',
+"Blue" => 'C:/Users/Andrew/.atom/.learn-ide/home/aaj3f/jukebox-cli-online-web-prework/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => 'C:/Users/Andrew/.atom/.learn-ide/home/aaj3f/jukebox-cli-online-web-prework/audio/Emerald-Park/07.mp3'
+ }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:\n - help : displays this help message\n - list : displays a list of songs you can play\n - play : lets you choose a song to play\n - exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  my_songs.each do |k, v|
+    puts "#{k}"
+  end
 end
 
+binding.pry
 
 def play(my_songs)
   #this method is slightly different!
@@ -33,7 +38,7 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
