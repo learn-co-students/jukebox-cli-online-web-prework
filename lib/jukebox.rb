f47c2d3b01c1fa0319d056965568dfcdec19da83
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 06:01:20 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +10,19 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help
+puts "help"
+puts "list"
+puts "play"
+puts "exit"
+end
+def play(songs)
 
+    if  songs.include?(songs) == false
+    puts "Invalid input, please try again"
+     else
+      songs.each_with_index do |song, index|
+      puts "#{index+1}.  #{song}"
+    end
+  end
+end
