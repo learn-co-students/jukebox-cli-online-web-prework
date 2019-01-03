09661118f419beadbfbf5ccc0cc4d2f8c67803bf
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 16:27:34 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,13 +16,45 @@ puts "list"
 puts "play"
 puts "exit"
 end
-def play(songs)
-
-    if  songs.include?(songs) == false
-    puts "Invalid input, please try again"
-     else
-      songs.each_with_index do |song, index|
+def list(songs)
+    songs.each_with_index do |song, index|
       puts "#{index+1}.  #{song}"
     end
+end
+
+def play (songs)
+  puts "Please enter a song name or number:"
+  users_input = gets.chomp
+  if  (1..9).include?(users_input.to_i)
+    puts "Playing #{songs[users_input.to_i-1]}"
+      elsif songs.include?(users_input)
+          puts "Playing#{ users_input} "
+        else
+          puts "Invalid input, please try again"
+
+  end
+
+end
+
+def exit_jukebox
+  puts  "Goodbye"
+end
+def run
+  help
+  puts "Please enter a command:"
+  users_input= gets.chomp
+  until users_input == exit
+   puts "Please enter a command:"
+   if users_input == "list"
+     list(songs)
+      elsif  users_input == "play"
+        play (songs)
+      elsif  users_input == "help"
+            help
+          else
+        end
   end
 end
+
+#
+#
