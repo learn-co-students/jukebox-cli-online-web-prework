bf58cb7177918bf11a8c9fe658a6aaa7782e04af
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-20 20:34:27 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,45 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+ 
+# puts "Enter your name:"
+# users_name = gets.chomp
+ 
+# puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  
+  songs.each.with_index(1) do |song, index|
+    puts "#{index}. #{song}"
+  end
+  
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_selection = gets.chomp
+  
+  if !song_selection.to_i == 0?
+    binding.pry
+    if songs.include?(song_selection)
+      puts "Playing #{song_selection}"
+    end
+  elsif song_selection.to_i > 0
+    if list(songs).include?(song_selection)
+      puts "Playing #{songs[song_selection + 1]}"
+    end
+  else
+    puts "Invalid input, please try again"
+  end
+end
