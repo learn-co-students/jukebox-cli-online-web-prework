5dc3ac52fde8cc2498e275c262482f6b9e610aa8
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-26 04:29:40 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -13,15 +13,19 @@
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  give list of available songs
 end
 
 
@@ -33,13 +37,15 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  play a song given command or full song name
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  exit jukebox
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  start program script
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +10,55 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
 
+def list(array)
+  array.each_with_index do |song, i|
+      puts "#{i+1}.#{song}"
+end
+end
+
+def play(array)
+  list(array)
+  puts "Please enter a song name or number:"
+  next_track = gets.chomp.to_i
+  if next_track > 1 && next_track < 10
+    puts "Playing #{next_track}"
+  else
+    puts "Invalid input, please try again"
+end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  # help
+
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.strip
+    case input
+    when 'list'
+      list(songs)
+    when 'play'
+      list(songs)
+      play(songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+end
