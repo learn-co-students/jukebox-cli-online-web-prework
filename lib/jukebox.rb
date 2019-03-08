20bb82a34846c05895998f2e3e1575a25564a474
Darren L <darren.lou@gmail.com>
2019-03-08 21:12:07 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,6 +20,11 @@ songs = [
 # puts say_hello(users_name)
 
 def help
+  puts `I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program`
 end
 
 def play
