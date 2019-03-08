2c64785df220356e0f06836cd6ba0a9d7089aa82
Annie Phan <anniephan.atp@gmail.com>
2019-03-08 07:31:01 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,60 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_choice = gets.chomp
+  if (1..9).to_a.include?(song_choice.to_i)
+    puts "Playing #{songs[song_choice.to_i - 1]}"
+  elsif songs.include?(song_choice)
+    puts "Playing #{song_choice}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+
+
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts "#{i+1}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  while user_response != "exit"
+    case user_response
+    when "help"
+      help
+      puts "Please enter a command:"
+      user_response = gets.chomp
+    when "list"
+      list(songs)
+      puts "Please enter a command:"
+      user_response = gets.chomp
+    when "play"
+      play(songs)
+      puts "Please enter a command:"
+      user_response = gets.chomp
+    else
+      puts "Invalid input, please try again:"
+      user_response = gets.chomp
+    end
+  end
+  exit_jukebox
+end
