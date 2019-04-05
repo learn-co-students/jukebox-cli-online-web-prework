3204a754bbdeed60de135c6711b2aaba9337a7dc
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-05 07:18:12 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
@@ -14,7 +15,11 @@
 
 def help
   #this method should be the same as in jukebox.rb
-
+puts "I accept the following commands:",
+"- help : displays this help message",
+"- list : displays a list of songs you can play",
+"- play : lets you choose a song to play",
+"- exit : exits this program"
 end
 
 
@@ -22,6 +27,8 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  songs = my_songs.keys
+  a = songs.each.with_index {|val,index| puts "#{index+1} - #{val}"}
 end
 
 
