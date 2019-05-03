94c066e2a9a6da7e03198423fb4a72a96592b480
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-05-03 19:10:53 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,17 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+#practice
+
+def say_hello(name)
+  
+  "Hi #{name}!"
+  
+end   
+
+puts "Enter your name:"
+users_name = gets.chomp!
+
+puts say_hello(users_name)
+
+
